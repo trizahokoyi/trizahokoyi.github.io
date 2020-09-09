6e807e391d25b5733e757159cbfe1e5a52f72f7f
trizahokoyi <56598633+trizahokoyi@users.noreply.github.com>
2020-09-09 17:57:16 +0300

    my innitial commit
    my first commit

---
+++ contact-form.php
@@ -0,0 +1,16 @@
+
+<?PHP
+$email = $_POST["emailaddress"];
+$to = "you@youremail.com";
+$subject = "New Email Address for Mailing List";
+$headers = "From: $email\n";
+$message = "A visitor to your site has sent the following email address to be added to your mailing list.\n
+
+Email Address: $email";
+$user = "$email";
+$usersubject = "Thank You";
+$userheaders = "From: you@youremailaddress.com\n";
+$usermessage = "Thank you for subscribing to our mailing list.";
+mail($to,$subject,$message,$headers);
+mail($user,$usersubject,$usermessage,$userheaders);
+?>
